9425e6945c80a27b6301244e74a7a8cb1c9e7f82
Krishna Vamshi Kusuma <93475905+KrishnaOnline@users.noreply.github.com>
2024-04-30 16:19:58 +0530

    cors update

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: "https://kwikskill-by-kkv.vercel.app",
+        origin: ["https://kwikskill-by-kkv.vercel.app"],
         credentials: true,
     })
 );
